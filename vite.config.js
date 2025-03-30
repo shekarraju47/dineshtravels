25a1e725e2934141b5b2431a59e49fc0cb2b44df
shekar <shekarraju47@outlook.com>
2025-03-30 12:46:47 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
+
   plugins: [react(),
   tailwindcss(),
   ],
+
 
   server: {
     host: true,
