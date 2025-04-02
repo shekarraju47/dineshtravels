a1f3c1c98f8d3d68635a8684e2f47da494468742
shekar <shekarraju47@outlook.com>
2025-04-02 21:17:30 +0530

    compleated driven by trust

--- src/components/StatisticItem.tsx
+++ src/components/StatisticItem.tsx
@@ -10,11 +10,11 @@ const StatisticItem: React.FC<StatisticItemProps> = ({
   description,
 }) => {
   return (
-    <article className="flex-1 shrink self-stretch my-auto basis-0">
+    <article className="flex-1 shrink self-stretch basis-0">
       <h3 className="text-2xl font-bold leading-tight text-neutral-800">
         {number}
       </h3>
-      <p className="mt-1 leading-7 text-neutral-800">
+      <p className="mt-1 text-lg max-md:text-sm leading-7 text-neutral-800">
         {description.split("\n").map((line, i) => (
           <React.Fragment key={i}>
             {line}
