1eede7143bd88d5184fff60a2c45739c952410b0
shekar <shekarraju47@outlook.com>
2025-04-02 21:12:36 +0530

    change fdf

--- src/components/StatisticItem.tsx
+++ src/components/StatisticItem.tsx
@@ -14,7 +14,7 @@ const StatisticItem: React.FC<StatisticItemProps> = ({
       <h3 className="text-2xl font-bold leading-tight text-neutral-800">
         {number}
       </h3>
-      <p className="mt-1 text-lg leading-7 text-neutral-800">
+      <p className="mt-1 leading-7 text-neutral-800">
         {description.split("\n").map((line, i) => (
           <React.Fragment key={i}>
             {line}
